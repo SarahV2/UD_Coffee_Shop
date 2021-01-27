3ae5d515a0b3a6f9ff1156f1d9bb9b93764e8b41
Sarah <alahmadisr@gmail.com>
2021-01-27 22:46:31 +0300

    added new api route

--- routes/api/drinks.js
+++ routes/api/drinks.js
@@ -49,4 +49,37 @@ router.post(
   }
 );
 
+// @route   PATCH api/drinks/:id
+// @desc    Update drink's info
+// @access  Private (public as of now)
+
+router.patch(
+  '/:drink_id',
+  [
+    check('title', 'title is required').not().isEmpty(),
+    check('recipe', "drink's recipe is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const drink = await Drink.findById(req.params.drink_id);
+      if (!drink) {
+        return res.status(404).json({ message: 'Not found' });
+      }
+      drink.title=req.body.title;
+      drink.recipe=req.body.recipe;
+
+
+      const updated_drink = await drink.save();
+      return res.json(updated_drink);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Server Error' });
+    }
+  }
+);
+
 module.exports = router;
